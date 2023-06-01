b25a2c6ab4d4904841e7ac8cde2b9ce671a0c91d
cryptoJLee <chris.jin84@hotmail.com>
2023-05-31 19:10:10 -0700

    backend: implement version management

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,9 @@
 #[macro_use] extern crate diesel;
 
 use actix_cors::Cors;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, HttpResponse};
 use actix_service::Service;
+use futures::future::{Either, ok};
 
 mod jwt;
 mod views;
@@ -15,6 +16,7 @@ mod json_serialization;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    const ALLOWED_VERSION: &'static str = include_str!("./output_data.txt");
     HttpServer::new(|| {
         let cors = Cors::default().allow_any_origin()
             .allow_any_method()
@@ -22,9 +24,27 @@ async fn main() -> std::io::Result<()> {
         let app = App::new()
             .wrap_fn(|req, srv| {
                 println!("{}-{}", req.method(), req.uri());
-                let future = srv.call(req);
+                let passed: bool;
+                if req.path().contains(&format!("/{}/", ALLOWED_VERSION)) {
+                    passed = true;
+                } else {
+                    passed = false;
+                }
+                let end_result;
+                if passed == true {
+                    end_result = Either::Left(srv.call(req))
+                } else {
+                    let resp = HttpResponse::NotImplemented().body(
+                        format!("only {} API is supported", ALLOWED_VERSION)
+                    );
+                    end_result = Either::Right(
+                        ok(req.into_response(resp)
+                            .map_into_boxed_body()
+                        )
+                    );
+                }
                 async {
-                    let result = future.await?;
+                    let result = end_result.await?;
                     Ok(result)
                 }
             })
